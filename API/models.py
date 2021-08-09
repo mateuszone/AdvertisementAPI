9868ad38437f442b69ef17891778605164877a8b
Mateuszone <mathewczaja@gmail.com>
2021-08-09 17:05:11 +0200

    Installed drf, added app and rest_framework to apps in settings and created models

--- API/models.py
+++ API/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+
+class Offer(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    price = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
