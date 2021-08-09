626d9194d06e40d66265c4774a9281c8a7f6d6e0
Mateuszone <mathewczaja@gmail.com>
2021-08-09 17:57:14 +0200

    added basic edition to ADMIN API for Offer and Category model

--- API/admin.py
+++ API/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Offer
+
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    list_filter = ['name']
+
+
+@admin.register(Offer)
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'price', 'created_at', 'category']
