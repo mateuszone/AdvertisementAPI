7306fe4fb9b519b55d94cf65ce1df7d2469909e4
Mateuszone <mathewczaja@gmail.com>
2021-08-11 19:36:04 +0200

    update factories

--- API/factories.py
+++ API/factories.py
@@ -1,15 +1,19 @@
 from random import randint
 
+import factory
 from factory.django import DjangoModelFactory
 from factory.faker import Faker
 from factory import Sequence, PostGenerationMethodCall, sequence, RelatedFactory, RelatedFactoryList
 from factory import SubFactory
 
+
 from API.models import Category
 
 
 class CategoryFactory(DjangoModelFactory):
-    name = Faker('word')
+
+    id = None
+    name = None
 
     @sequence
     def name(n):
@@ -18,6 +22,14 @@ class CategoryFactory(DjangoModelFactory):
             return f'Category-{max_id + 1}'
         except Category.DoesNotExist:
             return f'Category-0'
+
+    @sequence
+    def id(n):
+        try:
+            current_id = Category.objects.latest('id').id
+            return current_id + 1
+        except Category.DoesNotExist:
+            return 1
 
     class Meta:
         model = 'API.Category'
@@ -30,10 +42,9 @@ class OfferFactory(DjangoModelFactory):
     title = Faker('word')
     description = Faker('paragraph')
     price = Faker('credit_card_security_code')
-    # category = Faker('CategoryFactory')
-    # category = RelatedFactory('API.factories.CategoryFactory', 'name', id=1)
     category = RelatedFactoryList('API.factories.CategoryFactory',
                                   'id'
+
                                   # size=lambda: randint(1, 5)
                                   )
 
