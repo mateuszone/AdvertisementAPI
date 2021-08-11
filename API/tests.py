3a3dd6e7fd296dab8e6720c61c40e92637a9def6
Mateuszone <mathewczaja@gmail.com>
2021-08-11 21:26:56 +0200

    finished simple tests with factory data

--- API/tests.py
+++ API/tests.py
@@ -6,13 +6,14 @@ from API.models import Category, Offer
 from rest_framework.test import APITestCase
 from rest_framework import status
 from API.serializers import CategorySerializer, OfferMIniSerializer, OfferSerializer
+from API.factories import CategoryFactory, OfferFactory
 
 
 class CategoryViewSetTestCase(APITestCase):
     list_url = reverse("Category-list")
 
     def setUp(self):
-        self.name = Category.objects.create(name="Insert1")
+        self.category = CategoryFactory()
 
     def test_category_list(self):
         response = self.client.get(self.list_url)
@@ -20,19 +21,18 @@ class CategoryViewSetTestCase(APITestCase):
         self.assertEqual(len(response.data), 1)
 
     def test_category_detail_retrieve(self):
-        response = self.client.get(reverse("Category-detail", kwargs={"pk": 1}))
+        response = self.client.get(reverse("Category-detail", kwargs={"pk": self.category.id}))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_profile_update(self):
-        response = self.client.put(reverse("Category-detail", kwargs={"pk": 1}),
+        response = self.client.put(reverse("Category-detail", kwargs={"pk": self.category.id}),
                                    {"name": "Insert"})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(json.loads(response.content),
                          {"id": 1, "name": "Insert"})
 
     def test_profile_delete(self):
-        response = self.client.delete(reverse("Category-detail", kwargs={"pk": 1}))
-        # print(response)
+        response = self.client.delete(reverse("Category-detail", kwargs={"pk": self.category.id}))
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
 
@@ -40,11 +40,8 @@ class OffersViewSetTestCase(APITestCase):
     list_url = reverse("Offers-list")
 
     def setUp(self):
-        category = Category.objects.create(name="Insert")
-        self.offer = Offer.objects.create(title="TestOffer",
-                                          description="description",
-                                          price=23.12,
-                                          category=category)
+        self.category = CategoryFactory()
+        self.offer = OfferFactory()
 
     def test_offer_list(self):
         response = self.client.get(self.list_url)
@@ -52,19 +49,17 @@ class OffersViewSetTestCase(APITestCase):
         self.assertEqual(len(response.data), 1)
 
     def test_offer_detail_retrieve(self):
-        response = self.client.get(reverse("Offers-detail", kwargs={"pk": 1}))
-        print(response.data)
+        response = self.client.get(reverse("Offers-detail", kwargs={"pk": self.offer.id}))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 6)
 
     def test_offer_update(self):
-        category = Category.objects.create(name="Insert1")
-        response = self.client.put(reverse("Offers-detail", kwargs={"pk": 1}),
+        response = self.client.put(reverse("Offers-detail", kwargs={"pk": self.offer.id}),
                                    {"title": "Insert", "description": "description",
-                                    "price": 23.11, "category": 1})
+                                    "price": 23.11, "category": self.category.id})
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_profile_delete(self):
-        response = self.client.delete(reverse("Offers-detail", kwargs={"pk": 1}))
+        response = self.client.delete(reverse("Offers-detail", kwargs={"pk": self.offer.id}))
         self.assertEqual(response.data, 'Offer successfully deleted')
